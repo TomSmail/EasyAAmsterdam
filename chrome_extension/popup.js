e74afdd7b6a0b56f1d80b51f1a6345ccc8e88dc1
TomSmail <thomas.c.smail@gmail.com>
2024-06-08 21:24:03 +0200

    fix: comment out random ass code

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,15 +1,15 @@
 import { getActiveTabURL } from "./utils.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.local.get('purchaseInfo', (result) => {
-    if (result.purchaseInfo) {
-      document.getElementById('info').textContent = JSON.stringify(result.purchaseInfo, null, 2);
-    }
-  });
+// document.addEventListener('DOMContentLoaded', () => {
+//   chrome.storage.local.get('purchaseInfo', (result) => {
+//     if (result.purchaseInfo) {
+//       document.getElementById('info').textContent = JSON.stringify(result.purchaseInfo, null, 2);
+//     }
+//   });
 
-  chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === 'purchaseInfo') {
-      document.getElementById('info').textContent = JSON.stringify(message.data, null, 2);
-    }
-  });
-});
+//   chrome.runtime.onMessage.addListener((message) => {
+//     if (message.type === 'purchaseInfo') {
+//       document.getElementById('info').textContent = JSON.stringify(message.data, null, 2);
+//     }
+//   });
+// });
