08efc654f742ddde887879bc0498655a394622b2
CharlieAram <charlie21aram@gmail.com>
2024-06-08 22:27:44 +0200

    Feat(background.js, content.js, manifest.js, popup.html, popup.js): Displays text content of webpage

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,15 +1,10 @@
 import { getActiveTabURL } from "./utils.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.local.get('purchaseInfo', (result) => {
-    if (result.purchaseInfo) {
-      document.getElementById('info').textContent = JSON.stringify(result.purchaseInfo, null, 2);
-    }
-  });
-
-  chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === 'purchaseInfo') {
-      document.getElementById('info').textContent = JSON.stringify(message.data, null, 2);
-    }
+document.getElementById('getText').addEventListener('click', () => {
+  console.log("Click")
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "getInnerText" }, (response) => {
+      document.getElementById('output').innerText = response.innerText;
+    });
   });
 });
