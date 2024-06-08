8aaa87bf0810e8b269f0c9ecaeae079ddc2267b6
TomSmail <thomas.c.smail@gmail.com>
2024-06-08 22:19:03 +0200

    feat: add open ai intergration that can slot in with chrome extension

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -13,3 +13,30 @@ import { getActiveTabURL } from "./utils.js";
 //     }
 //   });
 // });
+
+apiKey = 'ASK_TOM_FOR_KEY'
+
+function openAiRequest(innerText) {
+    prompt = `Get me the departure and arrival from this text: '${innerText}' and return it as JSON in the form: {departure: <city1>, arrival: <city2>}`;
+    console.log(`***PROMPT*** : \n ${prompt}`)
+    fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+        model: "gpt-4",
+        messages: [{"role": "user", "content": prompt}]
+    })
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+    console.log(data);
+    })
+    .catch(error => {
+    console.error('Error:', error);
+    });
+}
