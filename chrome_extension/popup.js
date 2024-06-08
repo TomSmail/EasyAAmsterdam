ed63ea9f7984b44f62115e318a25bfbf5c74afcb
TomSmail <thomas.c.smail@gmail.com>
2024-06-09 00:21:50 +0200

    feat: intermediate ai extension 2

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -4,20 +4,29 @@ document.getElementById('getText').addEventListener('click', () => {
   console.log("Click")
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { action: "getInnerText" }, (response) => {
+			console.log(response);
 			const innerText = response.innerText;
+			const prompt1 = `Extract the departure and arrival location from this text: ${innerText}' and just return it as JSON in the form: {departure: <city1>, arrival: <city2>}`;
       document.getElementById('output').innerText = innerText;
-			kilosOfCO2 = openAiRequest(innerText);
-			// INSERT CODE HERE TO ADD TO CHAIN
-			console.log(kilosOfCO2);
+			let locations = openAiRequest(prompt1);
+			locations = JSON.parse(locations)
+			if (typeof locations.departure !== undefined && typeof locations.arrivals !== undefined) {
+				console.log(locations);
+				const prompt2 = `Return the distance between these two places: ${locations.departure} and ${locations.arrivals} in kilometers and nothing else`
+				const distance = openAiRequest(prompt2);
+				console.log(distance);
+			} else {
+				console.error("GPT did not return correct output")
+			}
     });
   });
 });
 
 const apiKey = ''
 
-function openAiRequest(innerText) {
-	prompt = `Find the number of kilos generated between the departure and arrival from this text: ${value}' and just return it as JSON in the form: {flight_emissions: <emissions>}`;
-	console.log(`***PROMPT*** : \n ${prompt}`)
+function openAiRequest(prompt) {
+	console.log(JSON.parse('{\n"departure": "London Heathrow (LHR)",\n"arrival": "Amsterdam Schiphol (AMS)"\n}'));
+	console.log(`***PROMPT*** : \n ${prompt}`);
 	fetch('https://api.openai.com/v1/chat/completions', {
 	method: 'POST',
 	headers: {
@@ -26,7 +35,8 @@ function openAiRequest(innerText) {
 	},
 	body: JSON.stringify({
 			model: "gpt-4",
-			messages: [{"role": "user", "content": prompt}]
+			messages: [{"role": "user", "content": prompt}],
+			temperature: 0
 	})
 	})
 	.then(response => {
@@ -34,12 +44,8 @@ function openAiRequest(innerText) {
 	})
 	.then(data => {
 	console.log(data);
-	if (typeof data.flight_emissions !== 'undefined') {
-    // the variable is defined
-		return data.flight_emissions
-	}	else {
-		return 0
-	}
+	console.log(data.choices[0].message.content)
+	return data.choices[0].message.content;
 	})
 	.catch(error => {
 	console.error('Error:', error);
