f50ccbc15bec8fac175d2cc8c213f43c180b61f1
CharlieAram <charlie21aram@gmail.com>
2024-06-08 20:47:56 +0200

    Feat(chrome_extension/*): background sending message to content

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,13 +1,16 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension Installed');
-  });
-  
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'purchaseInfo') {
-      console.log('Purchase Info:', message.data);
-      // You can store or process the information here
-      chrome.storage.local.set({ purchaseInfo: message.data }, () => {
-        console.log('Purchase info saved.');
-      });
-    }
-  });
+// chrome.runtime.onInstalled.addListener(() => {
+//   console.log('Extension Installed');
+// });
+
+
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  console.log("Background on updated tabs: ", tabId, tab);
+
+  if (tab.url && tab.url.includes("fake")) {
+    console.log("Sending message to content.js");
+    chrome.tabs.sendMessage(tabId, {
+      type: "testMessage1"
+    });
+  }
+});
