7494ac49cdf7abf4a7b3c0720f8ea3f04c3b425b
CharlieAram <charlie21aram@gmail.com>
2024-06-08 21:22:28 +0200

    Feat(background.js, content.js): In theory content communicates back to background

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -3,14 +3,20 @@
 // });
 
 
-
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   console.log("Background on updated tabs: ", tabId, tab);
 
   if (tab.url && tab.url.includes("fake")) {
     console.log("Sending message to content.js");
     chrome.tabs.sendMessage(tabId, {
-      type: "testMessage1"
+      type: "fromBackground"
     });
+  }
+});
+
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const { type, value } = obj;
+  if (type === "fromContent") {
+    console.log("We hit background message listener!", value);
   }
 });
